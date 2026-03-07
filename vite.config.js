cfadaae05d52b4327193c8ecacc6ae7c738c17c2
Christian Camacho <chris.ca.dev@gmail.com>
2026-03-07 08:39:39 -0800

    feat: add favicon

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/favicon/*',
+          dest: '',
+        },
+      ],
+    }),
+  ],
   build: {
     outDir: 'static',
     assetsDir: '',
